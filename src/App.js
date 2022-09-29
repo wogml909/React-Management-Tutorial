a8ae23fec89d6fd2abf9a4bcb7804cc12532f135
Your Name <zz909@naver.com>
2022-09-29 20:08:57 +0900

    Creat Customer.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import React, { Component } from 'react';
 import Customer  from './components/Customer';
 import './App.css';
 
-const customer = {
+const customers = [
+  {
+  'id':1,
+  'image': 'http://placeimg.com/64/64/2',
   'name' : '홍길동',
   'birthday' : '990629',
   'gender' : '남자',
-  'job' : '대학생',
+  'job' : '대학생'
+},
+{
+  'id':2,
+  'image': 'http://placeimg.com/64/64/3',
+  'name' : '나라요',
+  'birthday' : '990920',
+  'gender' : '남자',
+  'job' : '공무원'
+},
+{
+  'id':3,
+  'image': 'http://placeimg.com/64/64/4',
+  'name' : '오다리',
+  'birthday' : '950920',
+  'gender' : '여자',
+  'job' : '직장인'
 }
-
-
+]
 
 class App extends Component {
   render() {
     return (
-      <Customer
-       name={customer.name}
-       birthday={customer.birthday}
-       gender={customer.gender}
-       job={customer.job}
-    />
+      <div>
+        {customers.map(c =>{ return(<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
+     </div>
     );
- }
+   }
 }
+        
+
 export default App;
